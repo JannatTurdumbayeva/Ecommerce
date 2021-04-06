8037fa0f9f9c998957915c7f5ebc68d4a79d94f4
Jannat <you@yourdomain.example.comjannelya@gmail.com>
2021-04-06 11:04:17 +0600

    first commit

---
+++ store/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+from .views import *
+
+
+urlpatterns = [
+    #Leave as empty string for base url
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+
+    path('update_item/', views.updateItem, name="update_item"),
+    path('search', SearchListView.as_view(), name='search')
+
+]
